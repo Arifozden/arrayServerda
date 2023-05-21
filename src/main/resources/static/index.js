5056958e731bd9ae3459d2b2bac500c1d46e5777
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-05-21 11:07:09 +0200

    bootstrap ile sayfa duzenlendi

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -17,7 +17,7 @@ function hepsiniGetir(){
 }
 
 function bilgiDuzenle(musteriler){
-    let yaz="<table><tr><th>Isim</th><th>Adres</th></tr>";
+    let yaz="<table class='responsive'><tr><th>Isim</th><th>Adres</th></tr>";
     for(const musteri of musteriler){
         yaz+="<tr><td>"+musteri.isim+"</td><td>"+musteri.adres+"</td></tr>";
     }
